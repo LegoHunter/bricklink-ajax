1c026db6ce3a3cca2b6576f3cbeebd1ab70728b9
tvattima <github@vattima.com>
2019-06-29 15:25:13 -0400

    Fixed 403 Forbidden error recently introduced by Bricklink stopping calls to ajax URIs

--- src/main/java/com/bricklink/api/ajax/configuration/BricklinkAjaxConfiguration.java
+++ src/main/java/com/bricklink/api/ajax/configuration/BricklinkAjaxConfiguration.java
@@ -1,10 +1,11 @@
 package com.bricklink.api.ajax.configuration;
 
 import com.bricklink.api.ajax.BricklinkAjaxClient;
+import com.bricklink.web.support.BricklinkWebService;
 import feign.Feign;
+import feign.httpclient.ApacheHttpClient;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
-import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,10 @@ import org.springframework.context.annotation.Configuration;
 public class BricklinkAjaxConfiguration {
 
     @Bean
-    public BricklinkAjaxClient bricklinkAjaxClient() {
+    public BricklinkAjaxClient bricklinkAjaxClient(BricklinkWebService bricklinkWebService) {
         return Feign
                 .builder()
-                .client(new OkHttpClient())
+                .client(new ApacheHttpClient(bricklinkWebService.getHttpClient()))
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
                 .logger(new Slf4jLogger(BricklinkAjaxClient.class))
