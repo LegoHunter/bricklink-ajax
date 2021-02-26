36c4f73484bf31eb6c947f68208ec3946407b83a
tvattima <github@vattima.com>
2021-02-25 22:16:08 -0500

    Adds getList()

--- src/main/java/com/bricklink/api/ajax/support/CatalogItemsForSaleResult.java
+++ src/main/java/com/bricklink/api/ajax/support/CatalogItemsForSaleResult.java
@@ -4,7 +4,9 @@ import com.bricklink.api.ajax.model.v1.AjaxResult;
 import com.bricklink.api.ajax.model.v1.ItemForSale;
 import lombok.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -17,4 +19,8 @@ public class CatalogItemsForSaleResult extends AjaxResult {
     private Integer rpp;
     private Integer pi;
     private List<ItemForSale> list;
+
+    public List<ItemForSale> getList() {
+        return Optional.ofNullable(list).orElse(Collections.emptyList());
+    }
 }
