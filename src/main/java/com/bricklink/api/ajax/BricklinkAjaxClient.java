8011c427f7f541c40af9eef1cd0e5417a8344da6
tvattima <github@vattima.com>
2023-06-18 11:42:06 -0400

    * Open Rewrite to springboot 2.7

--- src/main/java/com/bricklink/api/ajax/BricklinkAjaxClient.java
+++ src/main/java/com/bricklink/api/ajax/BricklinkAjaxClient.java
@@ -8,37 +8,41 @@ import feign.RequestLine;
 import java.util.Map;
 
 public interface BricklinkAjaxClient {
-    @RequestLine("GET /ajax/clone/search/searchproduct.ajax?" +
-            "q={q}&" +
-            "st={st}&" +
-            "cond={cond}&" +
-            "brand={brand}&" +
-            "type={type}&" +
-            "cat={cat}&" +
-            "yf={yf}&" +
-            "yt={yt}&" +
-            "loc={loc}&" +
-            "reg={reg}&" +
-            "ca={ca}&" +
-            "ss={ss}&" +
-            "pmt={pmt}&" +
-            "nmp={nmp}&" +
-            "color={color}&" +
-            "min={min}&" +
-            "max={max}&" +
-            "minqty={minqty}&" +
-            "nosuperlot={nosuperlot}&" +
-            "incomplete={incomplete}&" +
-            "showempty={showempty}&" +
-            "rpp={rpp}&" +
-            "pi={pi}&" +
-            "ci={ci}")
+    @RequestLine("""
+            GET /ajax/clone/search/searchproduct.ajax?\
+            q={q}&\
+            st={st}&\
+            cond={cond}&\
+            brand={brand}&\
+            type={type}&\
+            cat={cat}&\
+            yf={yf}&\
+            yt={yt}&\
+            loc={loc}&\
+            reg={reg}&\
+            ca={ca}&\
+            ss={ss}&\
+            pmt={pmt}&\
+            nmp={nmp}&\
+            color={color}&\
+            min={min}&\
+            max={max}&\
+            minqty={minqty}&\
+            nosuperlot={nosuperlot}&\
+            incomplete={incomplete}&\
+            showempty={showempty}&\
+            rpp={rpp}&\
+            pi={pi}&\
+            ci={ci}\
+            """)
     SearchProductResult searchProduct(@QueryMap Map<String, Object> params);
 
-    @RequestLine("GET /ajax/clone/catalogifs.ajax?" +
-            "itemid={itemid}&" +
-            "cond={cond}&" +
-            "rpp={rpp}")
+    @RequestLine("""
+            GET /ajax/clone/catalogifs.ajax?\
+            itemid={itemid}&\
+            cond={cond}&\
+            rpp={rpp}\
+            """)
     CatalogItemsForSaleResult catalogItemsForSale(@QueryMap Map<String, Object> params);
 
     //StoreInventoryDetailUpdateResult
